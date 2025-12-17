5184a937575271613528fa6bbaa593bdb6c9e4da
Min Kyaw <mintheinkyawprd@gmail.com>
2025-12-17 20:50:11 +0000

    Implement main method to demonstrate Dog class functionality

--- src/ie/atu/inheritance/Main.java
+++ src/ie/atu/inheritance/Main.java
@@ -2,4 +2,12 @@ package ie.atu.inheritance;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", 3, "Golden Retriever");
+        System.out.println("Dog name: " + dog.getName());
+        System.out.println("Dog age: " + dog.getAge());
+        System.out.println("Dog breed: " + dog.getBreed());
+        dog.makeSound();
+    }
+
 }
